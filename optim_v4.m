e605cf03e906816a20fd5ec077cc8e7834e008c4
Rohan <rhngla@gmail.com>
2018-12-12 11:34:38 -0800

    Cleanup

--- optim_v4.m
+++ optim_v4.m
@@ -2,111 +2,130 @@
 %colliding.
 
 
-function [final_pos] = optim_v4(IM,pos)
-pos = unique(pos,'rows');
+function [final_pos] = optim_v4(IM,init_pos)
+
+%Params:
+show_plots = true;
+cellbodyradius = 7;
+beta_G = 10;
+beta_H = 10;
+maxsteps = 100;
+
+
+init_pos = unique(init_pos,'rows');
 IM = IM./max(IM(:));
 
-nhoodsize = 7;
+%Image padding
+nhoodsize = cellbodyradius;
 padsize = [2*nhoodsize,2*nhoodsize];
 IM = padarray(IM,padsize);
-pos = pos+padsize;
-%IM = imgaussfilt(IM,5);
+init_pos = init_pos+padsize;
+x = init_pos(:,2);
+y = init_pos(:,1);
 
-xx = pos(:,2);
-yy = pos(:,1);
+if show_plots
+    hf = figure;clf(hf)
+    imagesc(IM);hold on
+    colormap('gray')
+    plot(y,x,'sr','MarkerSize',20);
+    caxis([0 0.3])
+    %Debug limits
+    xlim([1617,1838])
+    ylim([416,638])
+    drawnow
+end
 
-%Pad the input with 2*nhood
-figure(1);clf(1)
-imagesc(IM);hold on
-colormap('gray')
-plot(yy,xx,'sr','MarkerSize',20);
-drawnow
-
-beta = 10;
-beta_S = 10;
-nsteps = 100;
-
-sigma = 5;
+%Parameters for the gaussian filter around cellcenter
+sigma = cellbodyradius/2;
 c = 1./(2*pi*sigma^2).^0.5;
-
-dfx = 1;
-dfy = 1;
 
 %Define neighbourhood
 [nhoodX,nhoodY] = ndgrid(-nhoodsize:nhoodsize,-nhoodsize:nhoodsize);
-nhoodX=nhoodX(:);
-nhoodY=nhoodY(:);
-rem = nhoodX.^2+nhoodY.^2>nhoodsize^2;
+nhoodX=nhoodX(:)';
+nhoodY=nhoodY(:)';
+rem = nhoodX.^2+nhoodY.^2> nhoodsize^2;
 nhoodX(rem)=[];
 nhoodY(rem)=[];
-nhoodX = nhoodX';
-nhoodY = nhoodY';
-exit_steps = 0;
 
-cc = parula(nsteps);
-for n = 1:nsteps
-    if max(abs(dfx(:)))+max(abs(dfy(:))) > 10^-10
-        x2x2 = (xx-xx').^2;
-        y2y2 = (yy-yy').^2;
-        
-        x2x = xx-xx';
-        y2y = yy-yy';
-        
-        r = (x2x2+y2y2).^0.5;
-        
-        X = nhoodX+round(xx);
-        Y = nhoodY+round(yy);
-        ind = sub2ind(size(IM),X,Y);
-        
-        Fi   = c*sum(IM(ind).*exp(-1.*(((Y-yy).^2+(X-xx).^2))./(2*sigma.^2)),2);
-        Fs   = -1*sum(S(r),2);
-        
-        [dsx,dsy] = dS(x2x,y2y,r);
-        
-        dfx = c*sum(((X-xx)./sigma.^2).*IM(ind).*exp(-1.*(((Y-yy).^2+(X-xx).^2))./(2*sigma.^2)),2);
-        dfy = c*sum(((Y-yy)./sigma.^2).*IM(ind).*exp(-1.*(((Y-yy).^2+(X-xx).^2))./(2*sigma.^2)),2);
-        
-        dxx = beta*dfx - beta_S*dsx;
-        dyy = beta*dfy - beta_S*dsy;
-        
-        maxjump = 1;
-        dxx(abs(dxx)>maxjump) = sign(dxx(abs(dxx)>maxjump))*maxjump;
-        dyy(abs(dyy)>maxjump) = sign(dyy(abs(dyy)>maxjump))*maxjump;
-        
-        xx = xx + dxx;
-        yy = yy + dyy;
-        
-        
-        if mod(n,100)==0
-            disp([sum(Fi),sum(Fs)])
-            %figure(1);
-            %plot(yy,xx,'.','Color',cc(n,:),'MarkerSize',20);hold on
-            %drawnow;
-        end
-        
-        figure(1);
-        plot(yy,xx,'.','Color',cc(n,:),'MarkerSize',20);hold on
-        drawnow;
-        exit_steps=n;
+%Plotting
+cc = parula(maxsteps);
+
+%Initialize
+step = 1;
+not_converged = true;
+not_exceedmaxsteps = true;
+
+while not_exceedmaxsteps && not_converged
+    %Note: implicit bsxfun assumed (R2016b+)!
+    x2x = x-x';
+    y2y = y-y';
+    x2x2 = (x2x).^2;
+    y2y2 = (y2y).^2;
+    r = (x2x2+y2y2).^0.5;
+    
+    grid_x = nhoodX+round(x);
+    grid_y = nhoodY+round(y);
+    grid_ind = sub2ind(size(IM),grid_x,grid_y);
+    
+    I_gauss = IM(grid_ind).*exp(-1.*(((grid_y-y).^2+(grid_x-x).^2))./(2*sigma.^2));
+    
+    [H,dH_x,dH_y]  = repelfun(x2x,y2y,r);
+    
+    dG_x = c./sigma.^2*sum((grid_x-x).*I_gauss,2);
+    dG_y = c./sigma.^2*sum((grid_y-y).*I_gauss,2);
+    
+    dx = beta_G*dG_x - beta_H*dH_x;
+    dy = beta_G*dG_y - beta_H*dH_y;
+    
+    %For stability
+    maxjump = 1;
+    dx(abs(dx)>maxjump) = sign(dx(abs(dx)>maxjump))*maxjump;
+    dy(abs(dy)>maxjump) = sign(dy(abs(dy)>maxjump))*maxjump;
+    
+    x = x + dx;
+    y = y + dy;
+    
+    if mod(step,10)==0
+        G = c*sum(I_gauss,2);
+        Fitness = sum(G - H);
+        disp([Fitness, sum(G),sum(H)])
     end
     
-end
-plot(yy,xx,'sc','MarkerSize',20);
-final_pos = [yy - padsize,xx - padsize];
+    if show_plots
+        figure(hf);
+        plot(y,x,'.','Color',cc(step,:),'MarkerSize',20);hold on
+        drawnow;
+    end
+    
+    step = step+1;
+    
+    not_converged = max(abs(dG_x(:)))+max(abs(dG_y(:))) > 10^-10;
+    not_exceedmaxsteps = step<maxsteps;
 end
 
-function fx = S(r)
-k = 0.1;
-fx = 1./(k*(r.^2));
+if not_converged
+    disp('Did not converge')
+    disp(['Exit at max_steps: ',num2str(step)])
+else 
+    disp('Converged')
+    disp(['Exit at step: ',num2str(step)])
 end
 
-function [dsx,dsy] = dS(x2x,y2y,r)
+if show_plots
+    figure(hf)
+    plot(y,x,'sc','MarkerSize',20);
+    drawnow
+end
+final_pos = [y - padsize,x - padsize];
+end
+
+function [f,df_x,df_y]  = repelfun(x2x,y2y,r)
 k = 0.1;
 r = r+10e10*eye(size(r));
 r(r>10) = 10e10;
-%r(r==0) = 0.1;
 
-dsx = sum((1./k).*-1*r.^(-2).*2.*x2x,2);
-dsy = sum((1./k).*-1*r.^(-2).*2.*y2y,2);
+f = sum(1./(k*(r.^2)),2);
+df_x = sum((1./k).*-1*r.^(-2).*2.*x2x,2);
+df_y = sum((1./k).*-1*r.^(-2).*2.*y2y,2);
 end
 
