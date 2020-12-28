adc3addb8e841eec77fdccbd55e61a888764b789
Rohan <rhngla@gmail.com>
2020-12-28 11:03:35 -0800

    working zarr+subtile based prediction

--- refactor/utils/data.py
+++ refactor/utils/data.py
@@ -1,9 +1,60 @@
 import glob
-from aicsimageio import imread
+import zarr
+import dask.array as da
 import numpy as np
 import torch
 from torch.utils.data import Dataset,Sampler
+from aicsimageio import imread
 from pathlib import Path
+from timebudget import timebudget as time
+
+
+def convert_u16_to_u8(im_u16):
+    """Converts a uint16 image into a uint8 image after scaling. 
+    This function matches scripts used to produce uint8 .tif files from LIMS jp2 images. 
+    """
+    assert im_u16.dtype == 'uint16', "Input must be uint16"
+    return np.uint8(np.round(im_u16.astype(float)*(2**8)/(2**16-1)))
+
+
+def nonzero_row_col(zarr_file,verbose=True):
+    """Output the co-ordinates where the first non-zero rows and columns occur in a given zarr array. 
+    Column- and row-wise sum calculations take ~50s for a 30000 x 40000 x 3 zarr image.
+
+    Example:
+    ```
+    im_path = '~/Remote-AI-root/allen/programs/celltypes/workgroups/mct-t200/Molecular_Genetics_Daigle_Team/Elyse/Unet_WB_testing/547656/'
+    fname_list = glob.glob(im_path+'/*.zarr')
+    for fname in fname_list:
+        f = zarr.open(fname)
+        print(fname)
+        _ = nonzero_row_col(f)
+    ```
+    """
+
+    dx = da.from_array(zarr_file)
+
+    #Reduce channel dimension
+    dflat = dx.sum(axis=2)
+
+    #Calculate column non-zeros
+    colsum = dflat.sum(axis=0)
+    colsum = colsum.compute()
+    y = np.diff(colsum > 0)
+    miny = np.min(np.flatnonzero(y != 0))
+    maxy = np.max(np.flatnonzero(y != 0))
+
+    #Calculate row non-zeros
+    rowsum = dflat.sum(axis=1)
+    rowsum = rowsum.compute()
+    x = np.diff(rowsum > 0)
+    minx = np.min(np.flatnonzero(x != 0))
+    maxx = np.max(np.flatnonzero(x != 0))
+
+    if verbose:
+        print(f'x-limits: [{minx}, {maxx}],y-limits: [{miny}, {maxy}]. Image shape: {zarr_file.shape}]')
+    return minx, maxx, miny, maxy
+
 
 class Ai224_RG_Dataset(Dataset):
     """Dataset for training images
@@ -160,9 +211,8 @@ class RandomSampler(Sampler):
         return self.num_samples
 
 
-#==============================================================================
 class Pred_Ai224_RG_Dataset(Dataset):
-    """Dataset with only images, used for prediction with the mofdel
+    """Dataset with only images, used for prediction with the model
 
     Args:
         patch_size: patch_size of returned items
@@ -236,12 +286,12 @@ class Pred_Ai224_RG_Dataset(Dataset):
         idx = torch.as_tensor(idx)
         return {'im':im_item, 'idx':idx[-2:]}
 
+
 class Pred_Sampler(Sampler):
     r"""Returns indices to sequentially generate patches as per dataset spec.
-    Use.
 
     Arguments:
-        dataset: 
+        dataset: object of a Dataset class
     """
 
     def __init__(self, dataset):
@@ -256,6 +306,116 @@ class Pred_Sampler(Sampler):
                 for j in range(self.n_y_patches):
                     #print(t, int(i*self.output_size),int(j*self.output_size))
                     yield([t, i*self.output_size, j*self.output_size])
+
+    def __len__(self):
+        return self.n_x_patches*self.n_y_patches
+
+
+class Pred_Ai224_RG_Zarr(Dataset):
+    """Read zarr dataset for prediction with the model
+
+    Args:
+        patch_size: patch_size of returned items
+        output_size: output size of the network
+        im_path: directory path with image .zarr files 
+        fname: name of image without channel extension (see default argument)
+        scale: pixel values are divided by this number
+    """
+
+    def __init__(self,
+                 patch_size = 260,
+                 output_size = 172,
+                 im_path='/home/rohan/Remote-AI-root/allen/programs/celltypes/workgroups/mct-t200/Molecular_Genetics_Daigle_Team/Elyse/Unet_WB_testing/547656/',
+                 fname=None,
+                 scale=1.0):
+        
+        super().__init__()
+        self.IM = zarr.open(im_path+fname)
+        self.IM = self.IM[15000:20000,10000:15000,:]
+        IM_shape = self.IM.shape[:2]
+        
+        n_x_patches = np.ceil(IM_shape[0]/output_size).astype(int)
+        n_y_patches = np.ceil(IM_shape[1]/output_size).astype(int)
+
+        #Not implementing padding for zarr files. 
+        #The images in this dataset already have enough padding around the tissue.
+        pad_xi = int((patch_size-output_size)/2)
+        pad_yi = int((patch_size-output_size)/2)
+        pad_xf = int(n_x_patches*output_size + (patch_size-output_size)/2 - IM_shape[0])
+        pad_yf = int(n_y_patches*output_size + (patch_size-output_size)/2 - IM_shape[1])
+        
+        self.n_tiles = self.IM.shape[0]
+        self.tile_shape_orig = IM_shape
+        self.tile_shape_padded = IM_shape
+        self.patch_size = patch_size
+        self.output_size = output_size
+        self.scale=float(scale)
+
+        self.pad_xi = pad_xi
+        self.pad_yi = pad_yi
+        self.pad_xf = pad_xf
+        self.pad_yf = pad_yf
+
+        self.n_x_patches = n_x_patches
+        self.n_y_patches = n_y_patches
+        self.dummy = np.zeros((2,self.patch_size,self.patch_size))
+        return
+
+    def __len__(self):
+        return self.n_tiles
+
+    def __getitem__(self, idx):
+        #idx is tuple with (t,x,y) co-ordinates defining the tile index, and current patch corner.
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        t,x,y = idx
+
+        #If out of bounds:
+        if (x+self.patch_size >= self.tile_shape_orig[0]) or (y+self.patch_size >= self.tile_shape_orig[1]):
+            return {'im':torch.as_tensor(self.dummy), 'idx':torch.as_tensor([x,y])}
+
+        #Ignore idx[0]: it specifies a tile. zarr based dataloader is expected to work with a single image section.
+        im_item = self.IM[x:x+self.patch_size, y:y+self.patch_size, :2]
+
+        #zarr files have uint16 data. only training and augmentation requires uint8.
+        im_item = im_item.astype(float)*(2**8)/(2**16-1)/self.scale
+
+        #zarr file shape (x,y,[red,green]), with 1:green and 0:red. Model requires ([green,red],x,y)
+        im_item = np.swapaxes(np.flip(im_item,axis=2),axis1=0,axis2=2).copy()
+
+        return {'im':torch.as_tensor(im_item), 'idx':torch.as_tensor([x,y])}
+
+
+class Pred_Sampler_Zarr(Sampler):
+    r"""Returns indices to sequentially generate patches as per zarr spec.
+     - tile -> subtile -> patch
+     - subtiles are defined so that prediction image -> xy co-ordinates encounters fewer patch boundaries.
+    
+    Arguments:
+        dataset: object of a Dataset class
+    """
+
+    def __init__(self, dataset, n_x_patch_per_subtile, n_y_patch_per_subtile):
+        self.n_tiles = int(dataset.n_tiles)
+        self.output_size = int(dataset.output_size)
+        self.n_x_patches = int(dataset.n_x_patches)
+        self.n_y_patches = int(dataset.n_y_patches)
+        self.n_x_patch_per_subtile = n_x_patch_per_subtile
+        self.n_y_patch_per_subtile = n_y_patch_per_subtile
+        self.n_x_subtiles = np.ceil(self.n_x_patches/self.n_x_patch_per_subtile).astype(int)
+        self.n_y_subtiles = np.ceil(self.n_y_patches/self.n_y_patch_per_subtile).astype(int)
+        
+        
+    def __iter__(self):
+        for t in range(self.n_tiles):
+            for i_x_subtile in range(self.n_x_subtiles):
+                for i_y_subtile in range(self.n_y_subtiles):
+                    for i in range(self.n_x_patch_per_subtile):
+                        for j in range(self.n_y_patch_per_subtile):
+                            yield [t,
+                                   (self.n_x_patch_per_subtile*i_x_subtile + i)*self.output_size,
+                                   (self.n_y_patch_per_subtile*i_y_subtile + j)*self.output_size]
 
     def __len__(self):
         return self.n_x_patches*self.n_y_patches
