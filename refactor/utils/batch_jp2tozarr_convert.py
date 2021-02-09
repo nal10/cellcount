295af6e50e687d04b6d99ac2dfaf894e39f8ad37
elmorin <elyselouisemorin@gmail.com>
2021-02-08 22:13:43 -0800

    avoid copying jp2s

--- refactor/utils/batch_jp2tozarr_convert.py
+++ refactor/utils/batch_jp2tozarr_convert.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
-import os
+import shutil,os
 import sys
 import glymur #conda install -c sunpy glymur
+from glob import glob
 import zarr
+
 glymur.set_option('lib.num_threads', 2)
 
-for file in os.listdir(str(sys.argv[1])):
-    if file.endswith(".jp2"):
-        print(file.split('.')[0])
-        jp2 = glymur.Jp2k(file)
-        zarr.save(str(sys.argv[1])+str(file.split('.')[0])+'.zarr', jp2[:])
+
+if not os.path.exists(str(sys.argv[2])+'/csv'):
+    os.makedirs(str(sys.argv[2])+'/csv')
+
+
+barcode = sys.argv[1].split('-')[0]
+jp2_list = glob(barcode+'-????_*/*-????.jp2')
+jp2_list.sort()
+
+for file_name in jp2_list:
+    if shutil.fnmatch.fnmatch(file_name, '*-????.jp2'):
+        print(file_name.split('.')[0].split('/')[-1])
+        jp2 = glymur.Jp2k(file_name)
+        zarr.save(str(sys.argv[2])+'/'+str(file_name.split('.')[0].split('/')[-1])+'.zarr', jp2[:])
